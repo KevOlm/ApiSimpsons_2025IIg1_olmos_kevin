75054080d48c795dca0ffb35221da7dd429d947e
KevOlm <k.olmos509@gmail.com>
2025-10-13 20:50:16 -0500

    yes2

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import { BrowserRoute} from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/ApiSimpsons_2025IIg1_olmos_kevin">
       <App />
-    </BrowserRouter>
   </StrictMode>,
 )
